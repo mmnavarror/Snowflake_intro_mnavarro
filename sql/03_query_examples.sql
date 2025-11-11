277c3ce30502a646b52680d1ae767a6de136051a
La Marga <mariamargarita.navarro@gmail.com>
2025-11-11 17:11:42 -0300

    Update 03_query_examples.sql

--- sql/03_query_examples.sql
+++ sql/03_query_examples.sql
@@ -1,1 +1,19 @@
+-- Productos en oferta
+SELECT nombre, precio_venta, precio_oferta,
+       ROUND((precio_venta - precio_oferta) / precio_venta * 100, 2) AS descuento_pct
+FROM productos
+WHERE precio_oferta > 0
+ORDER BY descuento_pct DESC;
+
+-- Productos sin oferta
+SELECT nombre, precio_venta
+FROM productos
+WHERE precio_oferta = 0;
+
+-- Promedio de descuento
+SELECT AVG(precio_venta - precio_oferta) AS descuento_promedio
+FROM productos
+WHERE precio_oferta > 0;
+
+
 
